f2a8826b225ca95942efb9dffcbf335bc02b1c74
bagaskaramadhan <bagaskaramadhan@gmail.com>
2020-11-17 01:28:35 +0700

    delete

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -3,32 +3,44 @@ const model = require('../models/book')
 const controller = {
     getAll: (req, res) => {
         model.findAll()
-        .then((result) => {
-            res.json(result)
-        })
-        .catch(err => {
-            console.log(err)
-        })
+            .then((result) => {
+                res.json(result)
+            })
+            .catch(err => {
+                console.log(err)
+            })
     },
     getOne: (req, res) => {
         const id = req.params.id
         model.findByPk(id)
-        .then((result) => {
-            res.json(result)
-        })
-        .catch(err => {
-            console.log(err)
-        })
+            .then((result) => {
+                res.json(result)
+            })
+            .catch(err => {
+                console.log(err)
+            })
     },
     insert: (req, res) => {
         const data = req.body
         model.create(data)
-        .then((result) => {
-            res.json(result)
+            .then((result) => {
+                res.json(result)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    },
+    deleted: (req, res) => {
+        const id = req.params.id
+        model.destroy({
+            where: { id }
         })
-        .catch(err => {
-            console.log(err)
-        })
+            .then((result) => {
+                res.json(result)
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }
 }
 
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { getAll, getOne, insert } = require('../controllers/book')
+const { getAll, getOne, insert, deleted } = require('../controllers/book')
 router
     .get('/book', getAll)
     .get('/detail/:id', getOne)
     .post('/insert', insert)
+    .delete('/destroy/:id', deleted)
 
 module.exports = router
